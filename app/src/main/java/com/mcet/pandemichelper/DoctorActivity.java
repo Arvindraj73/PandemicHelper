4225480ba60c6e28df803f8da5fd8c148ea97507
Arvindraj73 <arvindraj3172@gmail.com>
2020-07-23 00:30:45 +0530

    unorg

--- app/src/main/java/com/mcet/pandemichelper/DoctorActivity.java
+++ app/src/main/java/com/mcet/pandemichelper/DoctorActivity.java
@@ -93,7 +93,9 @@ public class DoctorActivity extends FragmentActivity implements OnMapReadyCallba
         history.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent i = new Intent(DoctorActivity.this, HistoryActivity.class);
+                i.putExtra("id", "unorg");
+                startActivity(i);
             }
         });
         setUpLocation();
